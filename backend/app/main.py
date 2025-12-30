a8c6f0bc86a2448333ef19c41ce32d224b767773
Partha-sudo1 <parthasarathy.g@codework.ai>
2025-12-30 14:23:36 +0530

    final push

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_origin_regex=r"https://.*\.vercel\.app|https://.*\.railway\.app",
+    allow_origin_regex=r"https://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
